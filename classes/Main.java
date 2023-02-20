1e4dcbc373f3eb50053820973f97074f5eaed71a
Sedomui12345 <93103859+Sedomui12345@users.noreply.github.com>
2023-02-20 13:20:15 +0800

    prepared main for windows addition

--- classes/Main.java
+++ classes/Main.java
@@ -1,7 +1,17 @@
 package classes;
 
+import javax.swing.JFrame;
+
 public class Main {
-	public static void main(String Args[]) {
-		
-	}
+    public static JFrame window;
+    public static void main(String[] args) {
+        System.out.println("menuWindowTest");
+        window = new Menu();
+    }
+    public static void gameWindow() {
+        window = new Game();
+    }
+    public static void settingsWindow() {
+        window = new Settings();
+    }
 }
